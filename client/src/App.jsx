9ce78f00cd70658c6ca63f173c9cc9b08f5da2fc
deepak4511 <deepaksingh451181@gmail.com>
2025-06-11 17:25:57 +0530

    adding updated code

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,24 +2,57 @@
 import './App.css'
 import Dashboard from './pages/Dashboard'
 import Login from './pages/login'
-import { Routes, Route, Navigate } from 'react-router-dom'
+import { Routes, Route, Navigate, useLocation, Outlet } from 'react-router-dom'
 import Task from './pages/Task'
+import TaskDetails from './pages/TaskDetails'
+import Trash from './pages/Trash'
+import Users from './pages/Users'
+
+
+
+function Layout(){
+  const user=""
+
+  const location = useLocation()
+
+  return user ? (
+    <div className='w-full h-screen flex flex-col md:flex-row'>
+      <div className='w-1/5 h-screen bg-wjite sticky top-0 hidden md:block'>
+    {/* <Sidebar/> */}
+      </div>
+      <MobileSidebar/>
+      <div className='flex-1 overflow-y-auto'>
+        <div className='p-4 2xl:px-10'>
+          <Outlet/>
+        </div>
+      </div>
+    </div>
+  ) : (
+    <Navigate to="/login" state={{ from : location }}/>
+  )
+
+}
 
 function App() {
 
   return (
     <main className='w-full min-h-screen bg-[#f3f4f6]'>
       <Routes>
-        <Route path="/login" element={<Login />} />
+        
         <Route element={<Layout/>}>
           <Route path="/" element={<Navigate to="/dashboard"/>} />
           <Route path='/dashboard' element={<Dashboard/>} />
           <Route path='/tasks' element={<Task/>}/>
-          <Route path='/completed/:status' element={<Task/>}/>
-          <Route path='/in-progress/:status' element={<Task/>}/>
-          <Route path='/tasks >' element={<Task/>}/>
+          <Route path='/completed/:status?' element={<Task/>}/>
+          <Route path='/in-progress/:status?' element={<Task/>}/>
+          <Route path='/todo/:status?' element={<Task/>}/>
+          <Route path='trashed' element={<Trash/>}/>
+          <Route path='/tasks/:id' element={<TaskDetails/>}/>
+          <Route path='/team'  element={<Users/>}/>
+          <Route path='/status'/>
           
         </Route>
+        <Route path='/login' element={<Login/>}/>
       </Routes>
     </main>
   )
--- client/src/pages/login.jsx
+++ client/src/pages/login.jsx
@@ -1,30 +1,61 @@
-import { Input } from '@/components/ui/input'
-import { Label } from '@/components/ui/label'
-import React from 'react'
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import React from "react";
+import { useForm } from "react-hook-form";
+import { useNavigate } from "react-router-dom";
 
 const Login = () => {
-  return (
-    <main className='w-full min-h-screen bg-[#f3f4f6]'>
-    <div></div>
-    <div className=''>
-        Welcome to the Login Page
-    </div>
-    <div className=''>
-        <form className='bg-white p-6 rounded shadow-md w-96'>
-            <h2 className='text-2xl font-bold mb-4'>Login</h2>
-            <div className='mb-4'>
-                <Label className='block text-gray-700 mb-2' htmlFor='email'>Email</Label>
-                <Input type='email' id='email' className='w-full p-2 border border-gray-300 rounded' placeholder='Enter your email' />
-            </div>
-            <div className='mb-4'>
-                <Label className='block text-gray-700 mb-2' htmlFor='password'>Password</Label>
-                <Input type='password' id='password' className='w-full p-2 border border-gray-300 rounded' placeholder='Enter your password' />
-            </div>
-            <button type='submit' className='w-full bg-blue-500 text-white p-2 rounded hover:bg-blue-600'>Login</button>
-        </form>
-    </div>
-    </main>
-  )
-}
+  const user = "";
 
-export default Login
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const navigate = useNavigate();
+
+  useEffect(())
+
+  return (
+    <main className="w-full min-h-screen bg-[#f3f4f6]">
+      <div></div>
+      <div className="">Welcome to the Login Page</div>
+      <div className="">
+        <form className="bg-white p-6 rounded shadow-md w-96">
+          <h2 className="text-2xl font-bold mb-4">Login</h2>
+          <div className="mb-4">
+            <Label className="block text-gray-700 mb-2" htmlFor="email">
+              Email
+            </Label>
+            <Input
+              type="email"
+              id="email"
+              className="w-full p-2 border border-gray-300 rounded"
+              placeholder="Enter your email"
+            />
+          </div>
+          <div className="mb-4">
+            <Label className="block text-gray-700 mb-2" htmlFor="password">
+              Password
+            </Label>
+            <Input
+              type="password"
+              id="password"
+              className="w-full p-2 border border-gray-300 rounded"
+              placeholder="Enter your password"
+            />
+          </div>
+          <button
+            type="submit"
+            className="w-full bg-blue-500 text-white p-2 rounded hover:bg-blue-600"
+          >
+            Login
+          </button>
+        </form>
+      </div>
+    </main>
+  );
+};
+
+export default Login;
